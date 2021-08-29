475608c169f1ad4142e3f518e8c3b42de5961ada
刘顺 <lsgggg123@gmail.com>
2021-08-29 21:30:13 +0800

    slf4j simple

--- slf4j-nop/src/main/java/com/lsgggg123/demo/slf4j/nop/HelloWorld.java
+++ slf4j-nop/src/main/java/com/lsgggg123/demo/slf4j/nop/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.lsgggg123.demo.slf4j.simple;
+package com.lsgggg123.demo.slf4j.nop;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
---
+++ slf4j-simple/src/main/java/com/lsgggg123/demo/slf4j/simple/Slf4jSimple.java
@@ -0,0 +1,11 @@
+package com.lsgggg123.demo.slf4j.simple;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Slf4jSimple {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Slf4jSimple.class);
+        logger.info("slf4j simple");
+    }
+}
