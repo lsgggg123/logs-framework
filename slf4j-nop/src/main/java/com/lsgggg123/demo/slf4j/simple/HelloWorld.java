4c33706759cd9d7fa9a579945baaab96858bfecf
刘顺 <lsgggg123@gmail.com>
2021-08-29 21:01:41 +0800

    slf4j nop

---
+++ slf4j-nop/src/main/java/com/lsgggg123/demo/slf4j/simple/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lsgggg123.demo.slf4j.simple;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+        logger.info("Hello World");
+    }
+}
