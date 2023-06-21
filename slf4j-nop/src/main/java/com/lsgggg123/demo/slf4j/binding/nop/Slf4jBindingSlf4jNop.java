aafb5e8a712e078f3238512d2ff88100400a639f
刘顺 <lsgggg123@gmail.com>
2023-06-22 00:27:53 +0800

    repackage & rename

---
+++ slf4j-nop/src/main/java/com/lsgggg123/demo/slf4j/binding/nop/Slf4jBindingSlf4jNop.java
@@ -0,0 +1,11 @@
+package com.lsgggg123.demo.slf4j.binding.nop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Slf4jBindingSlf4jNop {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Slf4jBindingSlf4jNop.class);
+        logger.info("Hello World");
+    }
+}
--- slf4j-simple/src/main/java/com/lsgggg123/demo/slf4j/binding/simple/Slf4jBindingSlf4jSimple.java
+++ slf4j-simple/src/main/java/com/lsgggg123/demo/slf4j/binding/simple/Slf4jBindingSlf4jSimple.java
@@ -1,11 +1,11 @@
-package com.lsgggg123.demo.slf4j.simple;
+package com.lsgggg123.demo.slf4j.binding.simple;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Slf4jSimple {
+public class Slf4jBindingSlf4jSimple {
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(Slf4jSimple.class);
+        Logger logger = LoggerFactory.getLogger(Slf4jBindingSlf4jSimple.class);
         logger.info("slf4j simple");
         logger.debug("slf4j simple debug");
     }
