b1221406da062e2c3c5c9909994c9ff1e284ea20
刘顺 <lsgggg123@gmail.com>
2023-06-21 23:00:36 +0800

    jul demo

--- slf4j-binding-jul/src/main/java/com/lsgggg123/demo/slf4j/binding/jul/Slf4jBindingJul.java
+++ slf4j-binding-jul/src/main/java/com/lsgggg123/demo/slf4j/binding/jul/Slf4jBindingJul.java
@@ -1,11 +1,28 @@
 package com.lsgggg123.demo.slf4j.binding.jul;
 
+import com.lsgggg123.demo.slf4j.utils.LoggerImpl;
+import com.lsgggg123.demo.slf4j.utils.Slf4jUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.InputStream;
+import java.util.logging.LogManager;
+
 public class Slf4jBindingJul {
+
+    static {
+        try {
+            InputStream inputStream = Slf4jBindingJul.class.getClassLoader().getResourceAsStream("logging.properties");
+            LogManager logManager= LogManager.getLogManager();
+            logManager.readConfiguration(inputStream);
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(Slf4jBindingJul.class);
         logger.info("slf4j bind to java.util.logging");
+        LoggerImpl loggerImpl = Slf4jUtils.getLoggerImpl();
     }
 }
