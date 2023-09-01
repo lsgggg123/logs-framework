a16911e6c9d9afc70ff0a9c442af37685e0258f6
刘顺 <lsgggg123@gmail.com>
2023-09-02 01:01:47 +0800

    not support 2.x

--- slf4j-utils/src/main/java/com/lsgggg123/demo/slf4j/utils/Slf4jUtils.java
+++ slf4j-utils/src/main/java/com/lsgggg123/demo/slf4j/utils/Slf4jUtils.java
@@ -1,5 +1,6 @@
 package com.lsgggg123.demo.slf4j.utils;
 
+import org.slf4j.Logger;
 import org.slf4j.spi.LoggerFactoryBinder;
 
 import java.lang.reflect.Method;
@@ -13,16 +14,25 @@ public final class Slf4jUtils {
     private static final String STATIC_LOGGER_BINDER = "org.slf4j.impl.StaticLoggerBinder";
 
     public static LoggerImpl getLoggerImpl() {
-        try {
-            Class<?> clz = Class.forName(STATIC_LOGGER_BINDER);
-            Method method = clz.getDeclaredMethod("getSingleton");
-            Object instance = method.invoke(null);
-            LoggerFactoryBinder loggerFactoryBinder = (LoggerFactoryBinder) instance;
-            String loggerFactoryClassStr = loggerFactoryBinder.getLoggerFactoryClassStr();
-            return LoggerImpl.getLoggerImplByClassStr(loggerFactoryClassStr);
-        } catch (Exception e) {
-            return LoggerImpl.UNKNOWN;
+        String slf4jVersion = getSlf4jVersion();
+        if (slf4jVersion.startsWith("1.")) {
+            try {
+                Class<?> clz = Class.forName(STATIC_LOGGER_BINDER);
+                Method method = clz.getDeclaredMethod("getSingleton");
+                Object instance = method.invoke(null);
+                LoggerFactoryBinder loggerFactoryBinder = (LoggerFactoryBinder) instance;
+                String loggerFactoryClassStr = loggerFactoryBinder.getLoggerFactoryClassStr();
+                return LoggerImpl.getLoggerImplByClassStr(loggerFactoryClassStr);
+            } catch (Exception e) {
+                return LoggerImpl.UNKNOWN;
+            }
+        } else {
+            // todo
+            throw new UnsupportedOperationException("2.x not supported yet.");
         }
+    }
 
+    public static String getSlf4jVersion() {
+        return Logger.class.getPackage().getImplementationVersion();
     }
 }
